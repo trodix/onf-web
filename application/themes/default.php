4cd6832afb770bc1179f1cc3f336cb7469441808
Sebastien Vallet <sebastien.vallet89@gmail.com>
2018-02-08 13:19:44 +0100

    font-awesome ok

--- application/themes/default.php
+++ application/themes/default.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/assets/css/bootstrap.css">
-    <link rel="stylesheet" href="/assets/fonts/fontawesome.css">
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" type='text/css' href="/assets/css/bootstrap.css">
+    <link rel="stylesheet" type='text/css' href="/assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" type='text/css' href="/assets/css/style.css">
 
     <script type="text/javascript" src="<?= '/assets/js/lib/jquery.min.js' ?>"></script>
     <script type="text/javascript" src="<?= '/assets/js/lib/bootstrap.min.js' ?>"></script>
