812f70702650f79106336cf9f46e16cef2400930
Sebastien Vallet <sebastien.vallet89@gmail.com>
2018-04-24 15:47:22 +0200

    nouvelle version repo

--- onf/application/controllers/api.php
+++ onf/application/controllers/api.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class api extends CI_Controller {
 
+	public function __construct($config = 'rest')
+	{
+	    header('Access-Control-Allow-Origin: *');
+	    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	    parent::__construct();
+	}
+
 	public function index()
 	{
 
